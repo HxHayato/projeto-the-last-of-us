89dfba420d5043d54216bbb06239e86a2a69e8a9
HxHayato <127465821+HxHayato@users.noreply.github.com>
2023-03-11 22:54:11 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,56 @@
+/*
+
+    Objetivo - quando clicarmos no botão temos que mostrar a imagem de fundo correspondente
+
+    Pensar da visão do usuário primeiro, dps no código
+
+    -passo 1 - dar um jeito de pegar os elementos HTML dos botões 
+
+    -passo 2 - dar um jeito de identificar o clique do usuário no botão(evento)
+
+    -passo 3 - desmarcar o botão selecionado anterior
+
+    -passo 4 -marcar o botão clicado como se estivesse selecionada
+
+    -passo 5 -esconder a imagem anterior
+
+    -passo 6 - fazer aparecer a imagem correspondente ao botão clicado
+
+*/
+
+// Passo 1 - dar um jeito de pegar os elementos HTML dos botões
+
+const botoesCarrosel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+// Passo 2 - dar um jeito de identificar o clique do usuário no botão(evento)
+
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrossel(botao);
+
+        esconderImagemAtiva();
+
+        mostrarImagemDeFundo(indice);
+    });
+}); 
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
